5807abf895a063d0fa834d65120ec2c7e0380b9a
789Michael <allanalli@hotmail.com>
2018-03-26 23:24:25 +0200

    v0.2, slideshow, rettede menu

--- Script1.js
+++ Script1.js
@@ -1,5 +1,6 @@
 // JavaScript source code
 // Used to toggle the menu on small screens when clicking on the menu button
+
 function myFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
@@ -16,3 +17,31 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var slideIndex = 0;
+carousel();
+
+
+function carousel() {
+	slideIndex++;
+	showDivs(slideIndex);
+    setTimeout(carousel, 8000);    
+}
+
+function showDivs(n) {
+
+  var header = document.getElementById("home");
+  if (n > 3) {slideIndex = 1}  
+  
+  switch(slideIndex){
+ 	 case 1:
+	 header.style.backgroundImage = "url(slide1.jpg)";
+	 break;
+	 case 2:
+	 header.style.backgroundImage = "url(slide2.jpg)";
+	 break;
+	 case 3:
+	 header.style.backgroundImage = "url(slide3.jpg)";
+	 break;
+	}
+ }
